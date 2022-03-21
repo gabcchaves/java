9d83efe8cbdb42625528cbdd28c2af49e1a3a027
Gabriel Costa Chaves <gabcchaves@zohomail.com>
2022-03-21 13:29:11 +0000

    Write a program that reads a float number and prints it.

---
+++ variablesAndExpressions/E02.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class E02 {
+	public static void main(String args[]) {
+		Scanner scn = new Scanner(System.in);
+		System.out.println("Enter a float number: ");
+		float x = scn.nextFloat();
+		System.out.println("You entered " + x + ".");
+	}
+}
