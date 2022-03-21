ec9be5444f68c576a4a8b5e7b05151fa4563f211
Gabriel Costa Chaves <gabcchaves@zohomail.com>
2022-03-21 13:23:12 +0000

    Write a program that reads an integer and print it.

---
+++ variablesAndExpressions/E01.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class E01 {
+	public static void main(String args[]) {
+		Scanner scn = new Scanner(System.in);	
+		System.out.println("Enter an integer: ");
+		int x = scn.nextInt();
+		System.out.println(x);
+	}
+}
