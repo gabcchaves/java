362010bb6f7387467fe8fdd95d831457db6adb7c
Gabriel Costa Chaves <gabcchaves@zohomail.com>
2022-03-21 16:43:43 +0000

    Read a float number and print its fith part.

---
+++ variablesAndExpressions/E05.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class E05 {
+	public static void main(String args[]) {
+		Scanner scn = new Scanner(System.in);
+		System.out.println("Enter a float number: ");
+		float n = scn.nextFloat();
+		System.out.println("*1/5: " + n / 5);
+	}
+}
