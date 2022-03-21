86cff589801bff66136e9635c59aa97e5da64c69
Gabriel Costa Chaves <gabcchaves@zohomail.com>
2022-03-21 21:12:32 +0000

    Read an area in squared meters and convert it to acres.

--- variablesAndExpressions/E24.java
+++ variablesAndExpressions/E24.java
@@ -3,10 +3,10 @@ public class E24 {
 	public static void main(String args[]) {
 		Scanner scn = new Scanner(System.in);
 
-		System.out.printf("Enter a length in squared meters: ");
+		System.out.printf("Enter an area in squared meters: ");
 		double squareMeters = scn.nextDouble();
 		double acres = squareMeters * 0.000247;
 
-		System.out.printf("Length converted to acres: ~ %.6f.\n", acres);
+		System.out.printf("Area converted to acres: ~ %.6f.\n", acres);
 	}
 }
