dc70707fb23e0bf4a9cb742b842d36e033516e15
Gabriel Costa Chaves <gabcchaves@zohomail.com>
2022-05-19 20:46:46 -0300

    Done.

--- exercises/bank/controller/BankController.java
+++ exercises/bank/controller/BankController.java
@@ -47,4 +47,45 @@ public class BankController {
 	public Account insertAccount(Account account) throws RegisteredAccountException {
 		return accountRepo.insertAccount(account);
 	}
+
+	public Account insertAccount(Account account) throws RegisteredAccountException {
+		return accountRepo.insertAccount(account);
+	}
+
+	public Account queryAccount(String number) throws AccountNotFoundException {
+		return accountRepo.queryAccount(number);
+	}
+
+	public void removeAccount(Account account) throws AccountNotFoundException {
+		if (account.getBalance() == 0) {
+			accountRepo.removeAccount(account);
+		} else {
+			throw new IsufficientBalanceException();
+		}
+	}
+
+	public void deposit(String number, double amount) throws AccountNotFoundException {
+		Account account = accountRepo.queryAccount(number);
+		account.deposit(amount);
+	}
+
+	public void withdraw(String number, double amount) throws AccountNotFoundException, InsufficientBalanceException {
+		Account account = accountRepo.queryAccount(number);
+		account.withdraw(amount);
+	}
+
+	public void withdraw(String fromAccountNumber, String toAccountNumber, double amount) throws AccountNotFoundException, InsufficientBalanceExcetion {		
+		Account account1 = accountRepo.queryAccount(fromAccountNumber);
+		Account account2 = accountRepo.queryAccount(toAccountNumber);
+		account1.withdraw(amount);
+		account2.deposit(amount);
+	}
+
+	public List<Account> getAllAccounts() {
+		return accountRepo.getAll();
+	}
+
+	public List<Account> getAllAccounts(String id) {
+		return accountRepo.getAll(id);
+	}
 }
